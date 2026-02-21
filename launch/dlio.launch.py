1a9ace7a1de1e368983bd472ba5b1d78c688fc51
Martin Hoang <cuonghoangchi@gmail.com>
2026-02-21 17:39:00 +0800

    Small fix for /map topic
    Signed-off-by: Martin Hoang <cuonghoangchi@gmail.com>

--- launch/dlio.launch.py
+++ launch/dlio.launch.py
@@ -70,6 +70,7 @@ def generate_launch_description():
         parameters=[dlio_yaml_path, dlio_params_yaml_path],
         remappings=[
             ('keyframes', 'dlio/odom_node/pointcloud/keyframe'),
+            ('map', 'dlio/map_node/map'),
         ],
     )
 
